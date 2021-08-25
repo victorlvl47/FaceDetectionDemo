6873b63355c282dab9fae03a43640bda3d7f4b53
victor <victor.lvl47@gmail.com>
2021-08-24 19:40:12 -0600

    First Commit

---
+++ FaceDetectionBasics.py
@@ -0,0 +1,22 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture("videos/1.mp4")
+pTime = 0
+
+while True:
+    success, img = cap.read()
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 140), 
+            cv2.FONT_HERSHEY_PLAIN, 10, (0, 255, 0), 2)
+
+    # resize img
+    resized_img = cv2.resize(img, (640, 480))
+
+    cv2.imshow("Image", resized_img)
+    
+    cv2.waitKey(1)
